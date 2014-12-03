e07b2fcaad302ac0c7a70ef9191557853caf54e2
Sanjo <Deundiak@gmail.com>
2014-12-03 02:44:35 +0200

    Fixed Lab9 bug
    Signed-off-by: Sanjo <Deundiak@gmail.com>

--- Lab9/Lab9/Hyper.cpp
+++ Lab9/Lab9/Hyper.cpp
@@ -1,17 +1,11 @@
 #include "Hyper.h"
 #include "TMA.h"
 
-extern func2 answ;
-
-#include <fstream>
-#include <algorithm>
-std::ofstream sf("s.txt");
-
 std::vector<std::vector<double>> Hyper::Process(double s, func2 F,
     func u0, func u1, func u2, func v0,
     size_t Nt, size_t Nl, double period, double length)
 {
-    std::vector<std::vector<double>> u(Nt+1, std::vector<double>(Nl+1)); // first time, next x
+    std::vector<std::vector<double>> u(Nt+1, std::vector<double>(Nl+1));
 
     double h = length / Nl;
     double dt = period / Nt;
@@ -25,23 +19,18 @@ std::vector<std::vector<double>> Hyper::Process(double s, func2 F,
     u[1][Nl] = u2(dt);
     for (size_t n = 1; n < Nl; n++)
         u[1][n] = u[0][n] + dt*v0(h*n) + dt*dt / 2 * ((u[0][n + 1] - 2 * u[0][n] + u[0][n - 1]) / h / h +F(0, h*n));
-        //u[1][n] = answ(dt, n*h);
 
     double err = 0;
-    for (size_t k = 1; k < Nt; k++) // time loop
+    for (size_t k = 1; k < Nt; k++)
     {
         TMA tma(Nl + 1);
         tma.Iterate(0, 1, 0, u1((k + 1)*dt));
 
         for (size_t n = 1; n < Nl; n++)
-        {
             tma.Iterate(-s, q + 2 * s, -s,
                 2 * u[k][n]*q - u[k - 1][n]*q + (1 - 2 * s)*(u[k][n + 1] - 2 * u[k][n] + u[k][n - 1]) + s*(u[k - 1][n + 1] - 2 * u[k - 1][n] + u[k - 1][n - 1])
                 + (s*F((k + 1)*dt, n*h) + (1 - 2 * s)*F(k*dt, n*h) + s*F((k - 1)*dt, n*h))*h*h);
-                //2 * answ(k*dt, n*h) * q - answ((k - 1)*dt, n*h) * q + (1 - 2 * s)*(answ(k*dt, (n + 1)*h) - 2 * answ(k*dt, n*h) + answ(k*dt, (n - 1)*h))
-                //+ s*(answ((k - 1)*dt, (n + 1)*h) - 2 * answ((k - 1)*dt, n*h) + answ((k - 1)*dt, (n - 1)*h))
-                //+ F(k*dt, n*h) *h*h);
-        }
+
         tma.Iterate(0, 1, 0, u2((k + 1)*dt));
 
         u[k+1] = tma.Result();
--- Lab9/Lab9/main.cpp
+++ Lab9/Lab9/main.cpp
@@ -1,31 +1,25 @@
-// utt=uxx+F(t,x) 0<x<L=1
-// u(t,x) u(0,x)=u0(x), ut(0,x)=0
-// u(t,0)=u1(t), u(t,L)=u2(t)
-// u0=u(0,x)=(x+0.2)sin(M_PIx/2)
-// u1=u(t,0)=0
-// u2=u(t,L)=u(t,1)=1.2cos(M_PI*t)
-// F(t,x)=-M_PI/2cos(M_PI*t)*(2cos(M_PI*x/2)+M_PI*(x+0.2)sin(M_PI*x/2)
-// v0=du/dt(x,0)=0
-// ANSWER = (x+0.2)*sin(M_PI*x/2)*cos(M_PI*t)
-
 #include "Hyper.h"
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
 #include <fstream>
 
-func2 answ = [](double t, double x)->double{ return (x + 0.2)*sin(M_PI*x / 2)*cos(M_PI*t); };
-
 int Test()
 {
     size_t const Nt = 1000;
     size_t const Nl = 1000;
 
-    func2 F = [](double t, double x)->double{ return -M_PI*cos(M_PI*t)*(cos(M_PI*x / 2) + 3 / 4 * M_PI*(x + 0.2)*sin(M_PI*x / 2)); };
     func u0 = [](double x)->double{ return (x + 0.2)*sin(M_PI*x / 2); };
     func u1 = [](double t)->double{ return 0.; };
     func u2 = [](double t)->double{ return 1.2*cos(M_PI*t); };
     func v0 = [](double x)->double{ return 0.; };
+
+    func2 utt = [](double t, double x)->double{ return -M_PI*M_PI*(x + 0.2)*sin(M_PI*x / 2)*cos(M_PI*t); };
+    func2 uxx = [](double t, double x)->double{ return (-M_PI*M_PI/4*(x+0.2)*sin(M_PI*x/2)+M_PI*cos(M_PI*x/2))*cos(M_PI*t); };
+
+    func2 F = [utt, uxx](double t, double x)->double{ return utt(t,x)-uxx(t,x); };
+
+    func2 answ = [](double t, double x)->double{ return (x + 0.2)*sin(M_PI*x / 2)*cos(M_PI*t); };
 
     double period = 2, length = 1;
     double h = length / Nl;
@@ -35,8 +29,9 @@ int Test()
 
     std::ofstream out("output.txt");
 
-    out << std::left;
-    /*{
+    out << std::left << std::fixed;
+
+    {
         for (size_t k = 0; k < Nt + 1; k++)
         {
             double m = 0;
@@ -45,25 +40,25 @@ int Test()
                 double a1 = answ(k*dt, n*h);
                 double a2 = res[k][n];
                 double err = abs(a1-a2);
-                m = (m*n + err) / (n + 1);
+                m = std::max(m,err);
             }
             out << std::setw(4) << k*dt << '\t'
                 << std::setw(12) << m << std::endl;
         }
+    }
+
+    /*{
+        for (size_t k = 0; k < Nt + 1; k++)
+        {
+            double m = 0;
+            for (size_t n = 0; n < Nl + 1; n++)
+            if (abs(res[k][n]) > m)
+                m = abs(res[k][n]);
+
+            out << std::setw(4) << k*dt << '\t'
+                << std::setw(12) << m << std::endl;
+        }
     }*/
-
-    //{
-    //    for (size_t k = 0; k < Nt + 1; k++)
-    //    {
-    //        double m = 0;
-    //        for (size_t n = 0; n < Nl + 1; n++)
-    //        if (abs(res[k][n]) > m)
-    //            m = abs(res[k][n]);
-
-    //        out << std::setw(4) << k*dt << '\t'
-    //            << std::setw(12) << m << std::endl;
-    //    }
-    //}
 
     /*{
         for (size_t n = 0; n < Nl + 1; n++)
@@ -71,11 +66,21 @@ int Test()
             << std::setw(12) << res[Nt][n] << std::endl;
     }*/
 
-    {
+    /*{
         for (size_t n = 0; n < Nl + 1; n++)
             out << std::setw(4) << n*h << '\t'
             << std::setw(12) << answ(Nt*dt, n*h) - res[Nt][n] << std::endl;
-    }
+    }*/
+
+    /*for (size_t k = 0; k <= Nt; k++)
+    {
+        out << "t=" << k*dt << std::endl;
+        for (size_t n = 0; n <= Nl; n++)
+        {
+            out << n*h << '\t' << answ(k*dt, n*h) << '\t' << res[k][n] << '\t' << answ(k*dt, n*h) - res[k][n] << std::endl;
+        }
+        out << std::endl;
+    }*/
 
     out << std::endl;
 
